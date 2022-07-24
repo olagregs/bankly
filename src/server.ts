d8934b0173c43f6d70cc2f68185ce7ac08aaff63
Greg <gregoryflucena@gmail.com>
2022-07-24 14:35:50 -0300

    Containers configurated

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/user", (request, response) => {
+  return response.json("Running..");
+});
+
 app.listen(3333, () => console.log("Server is running on port 3333!"));
